94b770e7ff6c449166b31adc0cb4e27bc533c356
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-05-06 12:04:34 -0300

    chore: initialize spring boot project

---
+++ src/main/java/br/com/budgetboys/pf_finance_service/PfFinanceServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.budgetboys.pf_finance_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfFinanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfFinanceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/budgetboys/pf_finance_service/PfFinanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.budgetboys.pf_finance_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfFinanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
