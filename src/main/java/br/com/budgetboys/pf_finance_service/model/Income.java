47a18512c832bd3f554af0d919a7dcceecfa2edd
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-05-07 08:35:27 -0300

    refactor: apply decorators to income model class

--- src/main/java/br/com/budgetboys/pf_finance_service/model/Income.java
+++ src/main/java/br/com/budgetboys/pf_finance_service/model/Income.java
@@ -1,12 +1,22 @@
 package br.com.budgetboys.pf_finance_service.model;
 
 import br.com.budgetboys.pf_finance_service.model.enums.IncomeCategory;
+import jakarta.persistence.*;
 
 import java.util.UUID;
 
+@Entity
 public class Income {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "income_id", unique = true, nullable = false)
     private UUID id;
+
+    @Column(name = "income_amout", nullable = false)
     private double amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "income_category")
     private IncomeCategory category;
 }
