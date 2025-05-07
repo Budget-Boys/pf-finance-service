b49036b81576f79e042ba8d20eb1bec385400c59
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-05-07 19:00:05 -0300

    feat; add JPA class JPAExpenseEntity

---
+++ src/main/java/br/com/budgetboys/pf_finance_service/adapters/outbound/entities/JPAExpenseEntity.java
@@ -0,0 +1,37 @@
+package br.com.budgetboys.pf_finance_service.adapters.outbound.entities;
+
+import br.com.budgetboys.pf_finance_service.adapters.outbound.entities.enums.ExpenseCategory;
+import br.com.budgetboys.pf_finance_service.domain.expense.Expense;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "expense")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class JPAExpenseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "expense_id", nullable = false, unique = true)
+    private UUID id;
+
+    @Column(name = "expense_amount", nullable = false)
+    private double amount;
+
+    @Column(name = "expense_category", nullable = false)
+    private ExpenseCategory category;
+
+    public JPAExpenseEntity(Expense expense) {
+        this.id = expense.getId();
+        this.amount = expense.getAmount();
+        this.category = expense.getCategory();
+    }
+}
