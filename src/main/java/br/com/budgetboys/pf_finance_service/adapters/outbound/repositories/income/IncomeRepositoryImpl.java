90efd989648a6965bc540d37c301af7179ca3e19
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-05-07 16:42:58 -0300

    feat: implement, save, findById, findAll and delete in IncomeRepositoryImpl

--- src/main/java/br/com/budgetboys/pf_finance_service/adapters/outbound/repositories/income/IncomeRepositoryImpl.java
+++ src/main/java/br/com/budgetboys/pf_finance_service/adapters/outbound/repositories/income/IncomeRepositoryImpl.java
@@ -1,11 +1,14 @@
 package br.com.budgetboys.pf_finance_service.adapters.outbound.repositories.income;
 
+import br.com.budgetboys.pf_finance_service.adapters.outbound.entities.JPAIncomeEntity;
 import br.com.budgetboys.pf_finance_service.domain.income.Income;
 import br.com.budgetboys.pf_finance_service.domain.income.IncomeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class IncomeRepositoryImpl implements IncomeRepository {
 
@@ -17,21 +20,27 @@ public class IncomeRepositoryImpl implements IncomeRepository {
 
     @Override
     public Income save(Income income) {
-        return null;
+        JPAIncomeEntity incomeEntity = new JPAIncomeEntity(income);
+        this.jpaIncomeRepository.save(incomeEntity);
+        return new Income(incomeEntity.getId(), incomeEntity.getAmount(), incomeEntity.getCategory());
     }
 
     @Override
     public Income findById(UUID id) {
-        return null;
+        Optional<JPAIncomeEntity> incomeEntity = this.jpaIncomeRepository.findById(id);
+        return incomeEntity.map(entity -> new Income(entity.getId(), entity.getAmount(), entity.getCategory())).orElse(null);
     }
 
     @Override
     public List<Income> findAll() {
-        return null;
+        return this.jpaIncomeRepository.findAll()
+                .stream()
+                .map(entity -> new Income(entity.getId(), entity.getAmount(), entity.getCategory()))
+                .collect(Collectors.toList());
     }
 
     @Override
     public void deleteById(UUID id) {
-
+        this.jpaIncomeRepository.deleteById(id);
     }
 }
