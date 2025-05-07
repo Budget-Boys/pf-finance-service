2a08c0a142d66b731773988a414e1ac39f4e4535
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-05-07 18:59:25 -0300

    feat: add repository interface ExpenseRepository

---
+++ src/main/java/br/com/budgetboys/pf_finance_service/domain/expense/ExpenseRepository.java
@@ -0,0 +1,11 @@
+package br.com.budgetboys.pf_finance_service.domain.expense;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ExpenseRepository {
+    Expense save(Expense expense);
+    Expense findById(UUID id);
+    List<Expense> findAll();
+    void deleteById(UUID id);
+}
