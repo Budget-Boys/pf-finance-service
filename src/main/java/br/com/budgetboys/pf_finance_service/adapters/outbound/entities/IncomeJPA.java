556db3bcad3f7e989cb5d6e25b73ecfe94f4fb09
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-05-07 12:08:24 -0300

    refactor: move JPA annotated class to adapters/outbound

---
+++ src/main/java/br/com/budgetboys/pf_finance_service/adapters/outbound/entities/IncomeJPA.java
@@ -0,0 +1,30 @@
+package br.com.budgetboys.pf_finance_service.adapters.outbound.entities;
+
+import br.com.budgetboys.pf_finance_service.adapters.outbound.entities.enums.IncomeCategory;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "income")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class IncomeJPA {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "income_id", unique = true, nullable = false)
+    private UUID id;
+
+    @Column(name = "income_amout", nullable = false)
+    private double amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "income_category")
+    private IncomeCategory category;
+}
