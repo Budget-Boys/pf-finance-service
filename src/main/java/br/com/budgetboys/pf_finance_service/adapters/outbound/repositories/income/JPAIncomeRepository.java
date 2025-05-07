c165d14c8c5bf8d07e5e306e420a6323d3ef1a99
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-05-07 16:31:52 -0300

    fix: correct JpaRepository generic type from JPAIncomeRepository to JPAIncomeEntity

--- src/main/java/br/com/budgetboys/pf_finance_service/adapters/outbound/repositories/income/JPAIncomeRepository.java
+++ src/main/java/br/com/budgetboys/pf_finance_service/adapters/outbound/repositories/income/JPAIncomeRepository.java
@@ -1,8 +1,9 @@
 package br.com.budgetboys.pf_finance_service.adapters.outbound.repositories.income;
 
+import br.com.budgetboys.pf_finance_service.adapters.outbound.entities.JPAIncomeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface JPAIncomeRepository extends JpaRepository<JPAIncomeRepository, UUID> {
+public interface JPAIncomeRepository extends JpaRepository<JPAIncomeEntity, UUID> {
 }
