217ef5840487a3437e74395382d8d0531cbebd2b
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-05-07 18:52:51 -0300

    feat: add new values to enum class ExpenseCategory

--- src/main/java/br/com/budgetboys/pf_finance_service/adapters/outbound/entities/enums/ExpenseCategory.java
+++ src/main/java/br/com/budgetboys/pf_finance_service/adapters/outbound/entities/enums/ExpenseCategory.java
@@ -1,5 +1,24 @@
 package br.com.budgetboys.pf_finance_service.adapters.outbound.entities.enums;
 
 public enum ExpenseCategory {
-
+    RENT,
+    MORTGAGE,
+    UTILITIES,
+    GROCERIES,
+    DINING_OUT,
+    DELIVERY,
+    FUEL,
+    SUPPLIES,
+    SUBSCRIPTIONS,
+    CLOTHING,
+    PERSONAL_CARE,
+    HEALTH,
+    ENTERTAINMENT,
+    TRAVEL,
+    HOBBIES,
+    PHONE,
+    INTERNET,
+    SOFTWARE,
+    CREDIT_CARD,
+    PETS,
 }
