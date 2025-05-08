0e71ce5ce32aad3db8aed647ec7d4dd97d3461a8
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-05-08 17:59:37 -0300

    feat: add findExpenseById method to ExpenseService for retrieving expenses by ID

--- src/main/java/br/com/budgetboys/pf_finance_service/adapters/outbound/Service/expense/ExpenseService.java
+++ src/main/java/br/com/budgetboys/pf_finance_service/adapters/outbound/Service/expense/ExpenseService.java
@@ -1,5 +1,7 @@
 package br.com.budgetboys.pf_finance_service.adapters.outbound.service.expense;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
 import br.com.budgetboys.pf_finance_service.domain.expense.Expense;
@@ -15,7 +17,11 @@ public class ExpenseService {
     }
 
     public Expense saveExpense(Expense expense){
-        
+
         return expenseRepository.save(expense);
+    }
+
+    public Expense findExpenseById(UUID id){
+        return expenseRepository.findById(id);
     }
 }
