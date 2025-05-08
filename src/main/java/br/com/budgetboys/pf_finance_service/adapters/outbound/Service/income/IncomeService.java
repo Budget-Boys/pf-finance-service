3ae7e3f9fd3069ce32534fc39b015114d1d548b8
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-05-08 20:12:17 -0300

    feat: implement IncomeService with repository integration for income management

--- src/main/java/br/com/budgetboys/pf_finance_service/adapters/outbound/Service/income/IncomeService.java
+++ src/main/java/br/com/budgetboys/pf_finance_service/adapters/outbound/Service/income/IncomeService.java
@@ -1,5 +1,18 @@
 package br.com.budgetboys.pf_finance_service.adapters.outbound.service.income;
 
+import org.springframework.stereotype.Service;
+
+import br.com.budgetboys.pf_finance_service.domain.income.Income;
+import br.com.budgetboys.pf_finance_service.domain.income.IncomeRepository;
+
+@Service
 public class IncomeService {
+
+    private final IncomeRepository incomeRepository;
+
+    public IncomeService (IncomeRepository incomeRepository){
+        this.incomeRepository = incomeRepository;
+    }
+    
     
 }
