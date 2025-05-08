101bf77aef2c2646a667bd96e0457f5176938710
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-05-08 17:17:01 -0300

    feat: implement ExpenseService with dependency injection for ExpenseRepository

--- src/main/java/br/com/budgetboys/pf_finance_service/adapters/outbound/Service/expense/ExpenseService.java
+++ src/main/java/br/com/budgetboys/pf_finance_service/adapters/outbound/Service/expense/ExpenseService.java
@@ -1,5 +1,15 @@
 package br.com.budgetboys.pf_finance_service.adapters.outbound.Service.expense;
 
+import org.springframework.stereotype.Service;
+
+import br.com.budgetboys.pf_finance_service.domain.expense.ExpenseRepository;
+
+@Service
 public class ExpenseService {
     
+    private final ExpenseRepository expenseRepository;
+
+    public ExpenseService (ExpenseRepository expenseRepository){
+        this.expenseRepository = expenseRepository;
+    }
 }
