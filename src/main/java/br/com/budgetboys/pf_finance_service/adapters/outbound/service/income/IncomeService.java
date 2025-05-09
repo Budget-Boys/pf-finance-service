e1c397c6716ce67862c630e95f65f2636765b35e
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-05-08 21:11:10 -0300

    feat: add methods to find income by ID and retrieve all incomes in IncomeService

--- src/main/java/br/com/budgetboys/pf_finance_service/adapters/outbound/service/income/IncomeService.java
+++ src/main/java/br/com/budgetboys/pf_finance_service/adapters/outbound/service/income/IncomeService.java
@@ -1,5 +1,8 @@
 package br.com.budgetboys.pf_finance_service.adapters.outbound.service.income;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
 import br.com.budgetboys.pf_finance_service.domain.income.Income;
@@ -20,5 +23,17 @@ public class IncomeService {
             throw new IllegalArgumentException("The expense amount cannot be negative");
         }
         return incomeRepository.save(income);
+    }
+
+    public Income findIncomeById(UUID id){
+        Income income = incomeRepository.findById(id);
+        if(income == null){
+            throw new IllegalArgumentException("Expense Id: " +id+ "not found");
+        }
+        return income;
+    }
+
+    public List<Income> getAllIncomes(){
+        return incomeRepository.findAll();
     }
 }
