b605c7c51c7edeabc9c197b16a2f1eeed284cdb6
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-05-20 19:35:22 -0300

    feat: create income controller class

---
+++ src/main/java/br/com/budgetboys/pf_finance_service/adapters/inbound/controller/income/IncomeController.java
@@ -0,0 +1,11 @@
+package br.com.budgetboys.pf_finance_service.adapters.inbound.controller.income;
+
+import br.com.budgetboys.pf_finance_service.adapters.outbound.repositories.income.IncomeRepositoryImpl;
+import br.com.budgetboys.pf_finance_service.domain.income.IncomeRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/income")
+public class IncomeController {
+}
