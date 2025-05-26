be039fd08d3d79d06cfb3f5e55aa97e2987046c8
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-05-25 21:57:48 -0300

    feat: add mapper toResponseDto and jpaToIncome

--- src/main/java/br/com/budgetboys/pf_finance_service/utils/mappers/IncomeMapper.java
+++ src/main/java/br/com/budgetboys/pf_finance_service/utils/mappers/IncomeMapper.java
@@ -1,7 +1,9 @@
 package br.com.budgetboys.pf_finance_service.utils.mappers;
 
+import br.com.budgetboys.pf_finance_service.adapters.outbound.entities.JPAIncomeEntity;
 import br.com.budgetboys.pf_finance_service.domain.income.Income;
 import br.com.budgetboys.pf_finance_service.domain.income.IncomeCreateDTO;
+import br.com.budgetboys.pf_finance_service.domain.income.IncomeResponseDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
@@ -17,4 +19,22 @@ public interface IncomeMapper {
             @Mapping(source = "dto.userId", target = "userId")
     })
     Income toEntity(IncomeCreateDTO incomeCreateDTO);
+
+    @Mappings({
+            @Mapping(source = "entity.id", target = "id"),
+            @Mapping(source = "entity.amount", target = "amount"),
+            @Mapping(source = "entity.category", target = "category"),
+            @Mapping(source = "entity.creationDate", target = "creationDate"),
+            @Mapping(source = "entity.userId", target = "userId")
+    })
+    IncomeResponseDTO toResponseDto(Income jpaIncomeEntity);
+
+    @Mappings({
+            @Mapping(source = "jpa.id", target = "id"),
+            @Mapping(source = "jpa.amount", target = "amount"),
+            @Mapping(source = "jpa.category", target = "category"),
+            @Mapping(source = "jpa.creationDate", target = "creationDate"),
+            @Mapping(source = "jpa.userId", target = "userId")
+    })
+    Income jpaToIncome(JPAIncomeEntity jpaIncomeEntity);
 }
