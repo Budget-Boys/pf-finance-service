84b2e979ab225bf3dfcf72c1c55d49532bd349ab
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-05-25 21:13:57 -0300

    feat: create income create dto class

---
+++ src/main/java/br/com/budgetboys/pf_finance_service/domain/income/IncomeCreateDTO.java
@@ -0,0 +1,21 @@
+package br.com.budgetboys.pf_finance_service.domain.income;
+
+import br.com.budgetboys.pf_finance_service.adapters.outbound.entities.enums.IncomeCategory;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+public class IncomeCreateDTO {
+
+    @Positive(message = "The income amount must be positive")
+    private double amount;
+
+    @NotNull(message = "The category cannot be null")
+    private IncomeCategory category;
+
+    private UUID userId;
+
+}
