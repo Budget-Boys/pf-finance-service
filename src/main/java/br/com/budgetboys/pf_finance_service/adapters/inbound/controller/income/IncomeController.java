b18b52d6f789047a369fb9da29f0e1bdcad023b4
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-05-26 20:12:50 -0300

    refactor: add endpoint path (income, create)

--- src/main/java/br/com/budgetboys/pf_finance_service/adapters/inbound/controller/income/IncomeController.java
+++ src/main/java/br/com/budgetboys/pf_finance_service/adapters/inbound/controller/income/IncomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/income")
+@RequestMapping("income")
 public class IncomeController {
 
     private final IncomeService incomeService;
@@ -19,7 +19,7 @@ public class IncomeController {
         this.incomeService = incomeService;
     }
 
-    @PostMapping
+    @PostMapping("create")
     public ResponseEntity<?> create(@RequestBody IncomeCreateDTO income) {
         try {
             IncomeResponseDTO savedIncome = this.incomeService.saveIncome(income);
