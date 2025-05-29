65f34d7674fe555571f414bf814411346c320144
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-05-29 16:15:49 -0300

    feat: implement user repository interface

---
+++ src/main/java/br/com/budgetboys/pf_finance_service/adapters/outbound/repositories/user/UserRepositoryImpl.java
@@ -0,0 +1,11 @@
+package br.com.budgetboys.pf_finance_service.adapters.outbound.repositories.user;
+
+import br.com.budgetboys.pf_finance_service.domain.user.UserCreateDTO;
+import br.com.budgetboys.pf_finance_service.domain.user.UserRepository;
+
+public class UserRepositoryImpl implements UserRepository {
+
+    @Override
+    public void save(UserCreateDTO user) {
+    }
+}
