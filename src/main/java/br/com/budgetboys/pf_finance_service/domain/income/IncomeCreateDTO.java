0d8af6794b67366d79721d910047d6e3deb971ec
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-06-03 07:21:22 -0300

    feat: add all constructor decorator to create income dto

--- src/main/java/br/com/budgetboys/pf_finance_service/domain/income/IncomeCreateDTO.java
+++ src/main/java/br/com/budgetboys/pf_finance_service/domain/income/IncomeCreateDTO.java
@@ -3,11 +3,13 @@ package br.com.budgetboys.pf_finance_service.domain.income;
 import br.com.budgetboys.pf_finance_service.adapters.outbound.entities.enums.IncomeCategory;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.UUID;
 
 @Getter
+@AllArgsConstructor
 public class IncomeCreateDTO {
 
     @Positive(message = "The income amount must be positive")
