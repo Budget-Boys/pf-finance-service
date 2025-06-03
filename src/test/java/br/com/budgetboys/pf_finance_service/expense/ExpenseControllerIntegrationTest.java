2c1f77c05ec3e23c7edfa9bdffb5cc709dd6aa0a
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-06-03 20:27:23 -0300

    refactor(test): make userId global variable in expense integration tests class

--- src/test/java/br/com/budgetboys/pf_finance_service/expense/ExpenseControllerIntegrationTest.java
+++ src/test/java/br/com/budgetboys/pf_finance_service/expense/ExpenseControllerIntegrationTest.java
@@ -2,6 +2,7 @@ package br.com.budgetboys.pf_finance_service.expense;
 
 import br.com.budgetboys.pf_finance_service.adapters.outbound.entities.enums.ExpenseCategory;
 import br.com.budgetboys.pf_finance_service.adapters.outbound.entities.enums.IncomeCategory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,7 +13,10 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import javax.print.attribute.standard.Media;
 
+import java.util.UUID;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,19 +28,26 @@ public class ExpenseControllerIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private UUID userId;
+
+    @BeforeEach
+    void setUp() {
+        this.userId = UUID.randomUUID();
+    }
+
     @Test
     void shouldCreateExpense() throws Exception {
-        String json = """
+        String json = String.format("""
                     {
                         "amount": 100.0,
                         "category": "FUEL",
-                        "userId": "68787c92-64c1-4303-9669-7f77f8b52489"
+                        "userId": "%s"
                     }
-                """;
+                """, userId);
         mockMvc.perform(post("/expenses")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath(    "$.category").value("FUEL"));
+                .andExpect(jsonPath("$.category").value("FUEL"));
     }
 }
