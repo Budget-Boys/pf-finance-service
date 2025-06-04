e4cdc232908e7ae59fd042fc18feaeda8e5085f8
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-06-04 09:54:13 -0300

    feat(test): add test to find all expenses and find by id

--- src/test/java/br/com/budgetboys/pf_finance_service/expense/ExpenseControllerIntegrationTest.java
+++ src/test/java/br/com/budgetboys/pf_finance_service/expense/ExpenseControllerIntegrationTest.java
@@ -2,14 +2,15 @@ package br.com.budgetboys.pf_finance_service.expense;
 
 import br.com.budgetboys.pf_finance_service.adapters.outbound.entities.enums.ExpenseCategory;
 import br.com.budgetboys.pf_finance_service.adapters.outbound.entities.enums.IncomeCategory;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.JsonPathResultMatchers;
 
 import javax.print.attribute.standard.Media;
 
@@ -23,12 +24,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ExpenseControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     private UUID userId;
+    private static String expenseId;
 
     @BeforeEach
     public void setUp() {
@@ -36,6 +39,7 @@ public class ExpenseControllerIntegrationTest {
     }
 
     @Test
+    @Order(1)
     void shouldCreateExpense() throws Exception {
         String json = String.format("""
                     {
@@ -50,4 +54,25 @@ public class ExpenseControllerIntegrationTest {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath(    "$.category").value("FUEL"));
     }
+
+    @Test
+    @Order(2)
+    void shouldFindAllExpenses() throws Exception {
+        String responseBody = mockMvc.perform(get("/expenses"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        expenseId = JsonPath.read(responseBody, "$[0].id");
+    }
+
+    @Test
+    @Order(3)
+    void shouldFindExpenseById() throws Exception {
+        mockMvc.perform(get("/expenses/{id}", expenseId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.amount").value(100.0));
+    }
+    
 }
