e9c1d18b0c8af0559484d9107e0e6bbf4afaeaf7
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-06-04 19:51:44 -0300

    merge: resolve conflicts

--- src/test/java/br/com/budgetboys/pf_finance_service/income/IncomeControllerIntegrationTest.java
+++ src/test/java/br/com/budgetboys/pf_finance_service/income/IncomeControllerIntegrationTest.java
@@ -1,4 +1,4 @@
-package br.com.budgetboys.pf_finance_service.expense;
+package br.com.budgetboys.pf_finance_service.income;
 
 import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.*;
@@ -11,9 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -21,13 +19,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class ExpenseControllerIntegrationTest {
+public class IncomeControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     private UUID userId;
-    private static String expenseId;
+    private static String incomeId;
 
     @BeforeEach
     void setUp() {
@@ -36,46 +34,45 @@ public class ExpenseControllerIntegrationTest {
 
     @Test
     @Order(1)
-    void shouldCreateExpense() throws Exception {
+    void shouldCreateIncome() throws Exception {
         String json = String.format("""
                     {
-                        "amount": 100.0,
-                        "category": "FUEL",
+                        "amount": "100.0",
+                        "category": "INVESTMENTS",
                         "userId": "%s"
                     }
                 """, userId);
-        mockMvc.perform(post("/expenses")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath(    "$.category").value("FUEL"));
+        mockMvc.perform(post("/incomes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.category").value("INVESTMENTS"));
     }
 
     @Test
     @Order(2)
-    void shouldFindAllExpenses() throws Exception {
-        String responseBody = mockMvc.perform(get("/expenses"))
+    void shouldFindAllIncomes() throws Exception {
+        String responseBody = mockMvc.perform(get("/incomes"))
                 .andExpect(status().isOk())
                 .andReturn()
                 .getResponse()
                 .getContentAsString();
 
-        expenseId = JsonPath.read(responseBody, "$[0].id");
+        incomeId = JsonPath.read(responseBody, "$[0].id");
     }
 
     @Test
     @Order(3)
-    void shouldFindExpenseById() throws Exception {
-        mockMvc.perform(get("/expenses/{id}", expenseId))
+    void shouldFindIncomeBydId() throws Exception {
+        mockMvc.perform(get("/incomes/{id}", incomeId))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.amount").value(100.0));
     }
 
     @Test
     @Order(4)
-    void shouldDeleteExpense() throws Exception {
-        mockMvc.perform(delete("/expenses/{id}", expenseId))
+    void shouldDeleteIncome() throws Exception {
+        mockMvc.perform(delete("/incomes/{id}", incomeId))
                 .andExpect(status().isOk());
     }
-
 }
